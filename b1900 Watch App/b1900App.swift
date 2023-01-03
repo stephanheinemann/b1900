5859882d22ba3398d61e14a7cfe7df3411859fea
Stephan Heinemann <stephan.heinemann@hotmail.com>
2023-01-02 17:54:38 -0800

    Initial Commit

---
+++ b1900 Watch App/b1900App.swift
@@ -0,0 +1,17 @@
+//
+//  b1900App.swift
+//  b1900 Watch App
+//
+//  Created by Stephan Heinemann on 2023-01-02.
+//
+
+import SwiftUI
+
+@main
+struct b1900_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
